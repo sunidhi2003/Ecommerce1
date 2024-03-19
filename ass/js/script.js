25841d59894f2324b447b475b37ac176d8566173
sunidhi2003 <sunidhiagg7@gmail.com>
2024-03-19 20:45:05 +0530

    add files

--- ass/js/script.js
+++ ass/js/script.js
@@ -59,7 +59,7 @@ homeSwiper.on("slideChangeTransitionEnd", () => {
 let circleText = circleBtn.querySelector(".circle-text");
 circleText.innerHTML = circleText.textContent
     .split("")
-    .map((char, index) => `<span style="transform: rotate(${index * 28.3}deg)">${char}</span>`)
+    .map((char, index) => <span style="transform: rotate(${index * 28.3}deg)">${char}</span>)
     .join("");
 
 /* ============== New Section ============== */
